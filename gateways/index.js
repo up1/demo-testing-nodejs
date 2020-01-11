5bb270c8a5b5dce7aef689f70dc824042a08419c
up1 <somkiat.p@gmail.com>
2020-01-11 09:50:03 +0700

    Step 4 :: gateway testing with nock

---
+++ gateways/index.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+const getDataFromExternalApi = async() => {
+    const result = await axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then(response => {
+        return response.data
+    })
+    .catch(error => {
+        console.log(error);
+    });
+    return result;
+}
+
+  module.exports = {
+    getDataFromExternalApi
+};
